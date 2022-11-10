013e765ad15be157f975f83dc5d73fa7cc96e80b
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-09 23:54:54 -0700

    Finish barebones multiselect logic, DRYing code needed

--- src/components/TrialSearch/TrialSearch.tsx
+++ src/components/TrialSearch/TrialSearch.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useRef } from 'react';
 import './TrialSearch.css';
 import useOnClickOutside from '../../useOnClickOutside';
 
@@ -7,9 +7,24 @@ interface TrialSearchProps {
 }
 
 const TrialSearch = ({ categories }: TrialSearchProps) => {
-
   const [selected, setSelected] = useState<string[]>([]);
   const [selectedAll, setSelectedAll] = useState<boolean>(false);
+
+  const categoryOptions = categories.map((category, index) => {
+    return (
+      <label key={index} className="categories-option">
+        <input 
+          className="category"
+          type="checkbox"
+          name={category}
+          checked={selected.includes(category)}
+          disabled={selectedAll}
+          onChange={(event) => toggleSelected(event.target.name)}
+        />
+        {category}
+      </label>
+    )
+  })
 
   const toggleSelected = (name: string) => {
     if (name === "all") {
@@ -35,32 +50,6 @@ const TrialSearch = ({ categories }: TrialSearchProps) => {
     });
   }
 
-  const disableInputs = () => {
-    const inputs = Array.from(
-      document.querySelectorAll('.category')
-    );
-    console.log('these are the disabled inputs', inputs)
-    inputs.forEach(input => {
-      input.setAttribute('disabled', '');
-    });
-  }
-
-  const categoryOptions = categories.map((category, index) => {
-    return (
-      <label key={index} className="categories-option">
-        <input 
-          className="category"
-          type="checkbox"
-          name={category}
-          checked={selected.includes(category)}
-          disabled={selectedAll}
-          onChange={(event) => toggleSelected(event.target.name)}
-        />
-        {category}
-      </label>
-    )
-  })
-
   const [showCategories, setShowCategories] = useState(false);
 
   const ref = useRef(null); 
@@ -68,11 +57,38 @@ const TrialSearch = ({ categories }: TrialSearchProps) => {
 
   useOnClickOutside(ref, () => setShowCategories(false));
 
+  let selectText;
+  if (!selected.length && !selectedAll) {
+    selectText = 'Select Category';
+  } else if (selectedAll) {
+    selectText = 'All Categories Selected';
+  } else if (selected.length === 1) {
+    selectText = '1 Category Selected';
+  } else {
+    selectText = `${selected.length} Catgories Selected`;
+  }
+
+  // rename these params etc. and make clearer!
+  const removeSelected = (name: string) => {
+    const filteredSelected = selected.filter(option => option !== name);
+    setSelected(filteredSelected)
+  }
+
+  const selectedOptions = selected.map((selection, index) => {
+    // make this its own component!
+    return (
+      <article key={index}>
+        <span onClick={() => removeSelected(selection)}>{selection} ❌</span>
+        {/* icon img of an x, delete from selected */}
+      </article>
+    )
+  })
+
   return (
     <>
-      <div className="select-category-btn" onClick={() => setShowCategories(true)}>
-        <span>Select Category</span>
-        <img src="https://cdn1.iconfinder.com/data/icons/arrows-vol-1-4/24/dropdown_arrow-1024.png" />
+      <div className="select-category" onClick={() => setShowCategories(true)}>
+        <span>{selectText}</span>
+        <img src="https://cdn1.iconfinder.com/data/icons/arrows-vol-1-4/24/dropdown_arrow-1024.png" alt="drop down arrow icon" />
       </div>
       {showCategories && 
       <div ref={ref} className="test-category-display">
@@ -87,6 +103,7 @@ const TrialSearch = ({ categories }: TrialSearchProps) => {
         </label>
         {categoryOptions}
       </div>}
+      {selectedOptions}
     </>
   )
 }
