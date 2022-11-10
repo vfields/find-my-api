15d60dcdd43a6c413c7ff46afc39b68126db5787
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-10 13:02:08 -0700

    Implement search via state, not separate API calls

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,10 +4,25 @@ import Nav from '../Nav/Nav';
 import Search from '../Search/Search';
 import Container from '../Container/Container';
 
+interface Api {
+  API: string;
+  Description: string;
+  Auth: string;
+  HTTPS: boolean;
+  Cors: string;
+  Link: string;
+  Category: string;
+}
+
 function App() {
+  const [apis, setApis] = useState<Api[]>([])
   const [categories, setCategories] = useState<string[]>([]);
 
   useEffect(() => {
+    fetch(`https://api.publicapis.org/entries`)
+      .then(response => response.json())
+      .then(data => setApis(data.entries))
+    
     fetch('https://api.publicapis.org/categories')
       .then(response => response.json())
       .then(data => setCategories(data.categories))
@@ -20,6 +35,9 @@ function App() {
       <Search
         categories={categories}
       />
+      <h2>{apis.length} APIs Remain</h2>
+      <button>Show Me!</button>
+      {/* on button click, APIs are displayed! */}
       <Container />
     </main>
   );
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -10,6 +10,23 @@ const Search = ({ categories }: SearchProps) => {
   const [selected, setSelected] = useState<string[]>([]);
   const [selectedAll, setSelectedAll] = useState<boolean>(false);
   const [keyword, setKeyword] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    if (!selected.length && !selectedAll) {
+      // no categories selected
+    } else if (selected.length && !keyword) {
+      // categor(ies) selected, but no keywords
+    } else if (selected.length && keyword) {
+      // categor(ies) selected AND keyword entered
+    } else if (selectedAll && !keyword) {
+      // selected all categories, but no keyword entered
+    } else if (selectedAll && keyword) {
+      // selected all categories, AND keyword entered
+    }
+    // clear the form
+  }
 
   return (
     <form className="api-search-form">
@@ -27,6 +44,8 @@ const Search = ({ categories }: SearchProps) => {
         placeholder="Search By Keyword!"
         onChange={(event) => setKeyword(event.target.value)}
       />
+      <button type="submit" onClick={handleClick}>Find My Api!</button>
+      {error && <h2>{error}</h2>}
     </form>
   );
 }
