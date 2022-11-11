9fd8db0cceb29f8b94ea013150e008ab97fb4ee7
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-11 12:15:58 -0700

    Style landing page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,16 @@
 import { NavLink } from 'react-router-dom';
 import './LandingPage.css';
 
+const telescope = require('../../assets/telescope.png');
+
 const LandingPage = () => {
   return (
     <section className="landing-page">
-      <p>I am LandingPage.</p>
-      <NavLink exact to="/home"><button>Get Started</button></NavLink>
+      <img src={telescope} alt="golden outline of a telescope" />
+      <h1 className="header-text">Find My API</h1>
+      <h2 className="tag-text">Bootcamps are hard enough.<br />Finding an API should be easier!</h2>
+      <p className="sub-tag-text">Search over 1,000+ free, public APIs by keyword and category, save your favorites, and get started on your project faster!</p>
+      <NavLink exact to="/home"><button className="get-started-btn">Get Started</button></NavLink>
     </section>
   );
 }
